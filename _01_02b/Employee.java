384fee7f464c824f10c9073f92c69ff3ab8d8a9d
hulithala <118376948+hulithala@users.noreply.github.com>
2023-11-07 09:59:11 +0000

    variables

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -3,35 +3,36 @@ package _01_02b;
 public class Employee {
 
   public static void main(String[] args) {
-
+    int age = 29;
+    System.out.println(age);
     // Create a variable called age of type int and assign it the value 29.
 
     // Print the age variable to the console.
-
+    boolean isAManager = true;
     // Create a variable called isAManager of type boolean and assign it the value
     // true.
-
+    System.out.println(isAManager);
     // Print the isAManager variable to the console.
-
+    double yearsOfService = 2.5;
     // Create a variable called yearsOfService of type double and assign it the
     // value 2.5.
-
+    System.out.println(yearsOfService);
     // Print the yearsOfService variable to the console.
-
+    int baseSalary = 3000;
     // Create a variable called baseSalary of type int and assign it the value 3000.
-
+    int overtimePayment = 40;
     // Create a variable called overtimePayment of type int and assign it the value
     // 40.
-
+    int totalPayment = baseSalary + overtimePayment;
     // Create a variable called totalPayment of type int and assign it to the value
     // of baseSalary added to overtimePayment.
-
+    System.out.println(totalPayment);
     // Print the totalPayment variable to the console.
-
+    double firstBonus = 10.00, secondBonus = 22.00, thirdBonus = 35.00;
     // Create three variables all of type double on a single line.
     // They should be called firstBonus, secondBonus and thirdBonus and they should
     // be assigned the values 10.00, 22.00 and 35.00.
-
+    System.out.println(firstBonus + secondBonus + thirdBonus);
     // Print out the sum of the variables called firstBonus, secondBonus and
     // thirdBonus.
 
